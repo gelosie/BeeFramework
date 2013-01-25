394e0fb077508b3c786e8125eca8b4b49d7a6f4b
gelosie <gelosie.wang@gmail.com>
2013-01-25 21:29:53 +0800

    init ARC support branch
    make the codes of Example/WhatsBug for ARC support branch's basic codes

---
+++ ExampleArc/WhatsBugArc/WhatsBugArc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WhatsBugArc
+//
+//  Created by jun on 13-1-25.
+//  Copyright (c) 2013年 zhenian.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
